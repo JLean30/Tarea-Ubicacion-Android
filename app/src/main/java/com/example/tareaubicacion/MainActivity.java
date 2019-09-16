138f91864a8d6a2ec3fba1a57faeec191a4b2900
Jean <jlean30@protonmail>
2019-09-15 20:48:01 -0600

    first upload

---
+++ app/src/main/java/com/example/tareaubicacion/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.tareaubicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient;
+    private String permision;
+    private TextView latitude;
+    private TextView longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        permision = "android.permission.ACCESS_COARSE_LOCATION";
+        checkPermission(permision,1);
+
+
+    }
+
+    private void obteinLocation(){
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+
+                        if (location != null) {
+                            latitude.setText("latitude: "+ location.getLatitude());
+                            longitude.setText("longitude: "+ location.getLongitude());
+                        }
+                    }
+                });
+    }
+
+    // Function to check and request permission
+    public void checkPermission(String permission, int requestCode)
+    {
+
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(
+                MainActivity.this,
+                permission)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat
+                    .requestPermissions(
+                            MainActivity.this,
+                            new String[] { permission },
+                            requestCode);
+            obteinLocation();
+        }
+        else {
+            obteinLocation();
+
+        }
+    }
+}
